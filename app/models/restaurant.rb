693f1baac0afb4900ed1d8c7aa0caed0c4963a09
ZhiCong Ore <zhicong1989@gmail.com>
2024-05-24 18:12:57 +0800

    Added create reviews

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,10 @@
 class Restaurant < ApplicationRecord
+  CATEGORIES = %w[chinese italian japanese french belgian].freeze
+
   has_many :reviews, dependent: :destroy
   # must have name, address, category
   validates :name, :address, :category, presence: true
   # category belongs to ["chinese", "italian", "japanese", "french", "belgian"]
-  validates :category, inclusion: { in: %w[chinese italian japanese french belgian],
+  validates :category, inclusion: { in: CATEGORIES,
                                     message: '%<value>s is not a valid size' }
 end
