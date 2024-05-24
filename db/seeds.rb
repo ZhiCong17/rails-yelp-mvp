c4d95a27fa7d44434d754b9083574d81cec1e17b
ZhiCong Ore <zhicong1989@gmail.com>
2024-05-24 15:12:17 +0800

    added index action

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.destroy_all
+
+macdonald = { name: 'Macdonald', category: 'chinese', address: 'raffles place' }
+kfc = { name: 'KFC', category: 'italian', address: 'dhoby ghaut' }
+arnolds = { name: "Arnold's", category: 'japanese', address: 'city hall' }
+pizza_hut = { name: 'Pizza Hut', category: 'french', address: 'kovan' }
+burger_king = { name: 'Burger King', category: 'belgian', address: 'jurong east' }
+
+[macdonald, kfc, arnolds, pizza_hut, burger_king].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+end
